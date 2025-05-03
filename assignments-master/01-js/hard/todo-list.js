d8124f9761c966154f033fad4377eaa2e19d97a7
LearningDevil <124495300+LearningDevil@users.noreply.github.com>
2025-05-03 10:38:47 +0530

    Add to-do Hard Assignment

--- assignments-master/01-js/hard/todo-list.js
+++ assignments-master/01-js/hard/todo-list.js
@@ -11,7 +11,75 @@
 */
 
 class Todo {
+  constructor(){
+    this.task = [];
+  }
+  add(todo){
+    this.task.push(todo);
+    return this;
+
+  }
+  remove(indexOfTodo){
+    this.task.splice(indexOfTodo, 1);
+    return this;
+
+  }
+  update(index, updatedTodo){
+    if(index>=0 && index<this.task.length){
+      this.task.splice(index,1,updatedTodo);
+    }
+    return this;
+  }
+  getAll(){
+    return this.task;
+  }
+  get(indexOfTodo){
+    return this.task[indexOfTodo] || null;
+
+  }
+  clear(){
+    this.task = [];
+    return this;
+
+  }
 
 }
 
 module.exports = Todo;
+
+
+/* some extra experiments
+class pp{
+  constructor(){
+    this.result = [];
+  }
+  add(ap){
+      this.result.push(ap);
+  }
+  getTodo(todoindex){
+    return this.result.map((task, index) =>({
+      index: index,
+      task: task
+    }));
+  }
+  // getAll(){
+  //   this.result.forEach((value, index) =>{
+  //     console.log(
+  //     {index: index,
+  //     task: value
+  //     });
+  //   });
+  // }
+  getAll(){
+    return this.result.forEach((value)=>{
+      console.log(value);
+    });
+  }
+}
+const todoList = new pp();
+todoList.add('Task 1');
+todoList.add('Task 2');
+todoList.add('Task 3');
+todoList.getAll()
+*/
+
