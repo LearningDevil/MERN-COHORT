0fe4d471cd7be253e54b4f7163dccec5c8cabe70
LearningDevil <124495300+LearningDevil@users.noreply.github.com>
2026-01-08 17:25:57 +0530

    minor changes

---
+++ Week1/after-break.js
@@ -0,0 +1,84 @@
+// console.log("Hi there..");
+// // console.log(a);
+
+// function first(){
+//     var a =1;
+//     console.log(a);
+//     a = 3;
+//     console.log(a);
+//     let b = 1;
+//     console.log(b);
+//     if (a==2){
+//         console.log("blle blle..")
+//     }else if (b==1){
+//         console.log("la la lalalaaaa...")
+//     }
+//     for (let i =0; i<10; i++){
+//         console.log(i+1)
+//     }
+// }
+// // first()
+
+// function array_objects(){
+//     const a = [1,2,3,4];
+//     console.log(a)
+//     console.log(typeof(a))
+// }
+// array_objects()
+
+// class samosa{
+//     static wrapper(input){
+//         console.log(input)
+//     }
+//     static cpu_load(){
+//         let sum = 0
+//         for (let i = 0; i<10000000000000; i++){
+//             sum = sum + i;
+//         }
+//         console.log(sum)
+//     }
+// }
+
+// class sumsa{
+//     static sum(num1, num2) {
+//     let result = num1 + num2;
+//     return result;
+//     }
+
+//     static displayResult(data) {
+//     console.log("Result of the sum is : " + data);
+//     return data;
+//     }
+
+//     static displayResultPassive(data) {
+//     console.log("Sum's result is : " + data);
+//     }
+
+    
+// }
+// console.log(sumsa.displayResult(sumsa.sum(3, 5)));
+
+// function sum(num1, num2) {
+// let result = num1 + num2;
+// return result;
+// }
+
+// function displayResult(data) {
+// console.log("Result of the sum is : " + data);
+// return data;
+// }
+
+// function displayResultPassive(data) {
+// console.log("Sum's result is : " + data);
+// }
+
+// console.log(displayResultPassive(sum(3, 5)));
+
+// class stringimplementations{
+//     function strs(){
+        
+//     }
+// }
+
+
+console.log(parseInt("42jhghdf265"))
